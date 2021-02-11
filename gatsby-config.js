10ed421683a2559d26e893e8e0bb5c0ececffd44
Viranchee Lotia <viranchee@outlook.com>
2021-02-11 18:22:43 +0530

    Add ChakraUI

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "gatsby",
   },
-  plugins: [
-    "gatsby-transformer-remark",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+  plugins: ["@chakra-ui/gatsby-plugin"],
 };
